4576dc138e4eaddbbb8cd472f67cbbe630653747
incbyc <121228508+incbyc@users.noreply.github.com>
2025-05-02 13:42:39 +0200

    Add files via upload

---
+++ data/HomeSites.js
@@ -0,0 +1,108 @@
+var json_HomeSites = {
+    "type": "FeatureCollection",
+    "name": "HomeSites",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "Site": "Matitila",
+                "Family Code": "BC(BMS) 18",
+                "Contact No": "78017551",
+                "Region": "Hhohho",
+                "No of Children": 1.0
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    31.205388,
+                    -26.04755
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Site": "Nkhaba",
+                "Family Code": "BC(BMS) 01",
+                "Contact No": "76152519",
+                "Region": "Hhohho",
+                "No of Children": 2.0
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    31.150695,
+                    -26.173847
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Site": "Motshane",
+                "Family Code": "BC(BMS) 28",
+                "Contact No": "76051191",
+                "Region": "Hhohho",
+                "No of Children": 76051191.0
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    31.063729,
+                    -26.250556
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Site": "Zone 4",
+                "Family Code": "BC(BMS)10",
+                "Contact No": "76182814",
+                "Region": "Hhohho",
+                "No of Children": 1.0
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    31.121797,
+                    -26.335784
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Site": "Mahhwalala",
+                "Family Code": "BC(BMS)32",
+                "Contact No": "76765367",
+                "Region": "Hhohho",
+                "No of Children": 1.0
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    31.103732,
+                    -26.327569
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Site": "Luyengo",
+                "Family Code": "Ngwenyameni",
+                "Contact No": "BC(BMS)13",
+                "Region": "Manzni",
+                "No of Children": 1.0
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    31.3517,
+                    -27.13
+                ]
+            }
+        }
+    ]
+};
