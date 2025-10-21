d8768b9a0ce27406fcbf88a8f35232135d96c435
incbyc <121228508+incbyc@users.noreply.github.com>
2025-10-21 12:55:31 +0200

    Add files via upload

---
+++ BubeleCare_10.js
@@ -0,0 +1,25 @@
+var json_BubeleCare_10 = {
+"type": "FeatureCollection",
+"name": "BubeleCare_10",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+
+{ "type": "Feature", "properties": { "Site": "Matitila", "Family Code": "BC(BMS)18", "Region": "Hhohho" }, "geometry": { "type": "Point", "coordinates": [31.20538, -26.04755] } },
+{ "type": "Feature", "properties": { "Site": "Nkhaba", "Family Code": "BC(BMS)01", "Region": "Hhohho" }, "geometry": { "type": "Point", "coordinates": [31.150695, -26.07550] } },
+{ "type": "Feature", "properties": { "Site": "Zone 4", "Family Code": "BC(BMS)10", "Region": "Hhohho" }, "geometry": { "type": "Point", "coordinates": [31.121797, -26.335784] } },
+{ "type": "Feature", "properties": { "Site": "Motshane", "Family Code": "BC(BMS)28", "Region": "Hhohho" }, "geometry": { "type": "Point", "coordinates": [31.063729, -26.250556] } },
+{ "type": "Feature", "properties": { "Site": "Mahwalala", "Family Code": "BC(BMS)18", "Region": "Hhohho" }, "geometry": { "type": "Point", "coordinates": [31.121797, -26.327569] } },
+{ "type": "Feature", "properties": { "Site": "Luyengo", "Family Code": "BC(BMS)13", "Region": "Manzini" }, "geometry": { "type": "Point", "coordinates": [31.3517, -27.130] } },
+{ "type": "Feature", "properties": { "Site": "Maliyaduma", "Family Code": "BC(BMS)05", "Region": "Manzini" }, "geometry": { "type": "Point", "coordinates": [31.414532, -26.438297] } },
+{ "type": "Feature", "properties": { "Site": "Mankayane", "Family Code": "BC(BMS)F01", "Region": "Manzini" }, "geometry": { "type": "Point", "coordinates": [30.999813, -26.7346640] } },
+{ "type": "Feature", "properties": { "Site": "Ngcwaleni", "Family Code": "BC(BMS)48", "Region": "Lubombo" }, "geometry": { "type": "Point", "coordinates": [31.7125005, -26.73644341] } },
+{ "type": "Feature", "properties": { "Site": "Mbadlane", "Family Code": "BC(BMS)39", "Region": "Lubombo" }, "geometry": { "type": "Point", "coordinates": [31.7125005, -26.4438010] } },
+{ "type": "Feature", "properties": { "Site": "Siteki Moyeni", "Family Code": "BC(BMS)88", "Region": "Lubombo" }, "geometry": { "type": "Point", "coordinates": [31.9556150, -26.4546010] } },
+{ "type": "Feature", "properties": { "Site": "Siphofaneni", "Family Code": "BC(BMS)F02", "Region": "Lubombo" }, "geometry": { "type": "Point", "coordinates": [31.7093250, -26.67206001] } },
+{ "type": "Feature", "properties": { "Site": "Mkhuzweni", "Family Code": "BC(BMS)61", "Region": "Hhohho" }, "geometry": { "type": "Point", "coordinates": [31.3418244, -25.8916178] } },
+{ "type": "Feature", "properties": { "Site": "Msahweni", "Family Code": "BC(BMS)81", "Region": "Hhohho" }, "geometry": { "type": "Point", "coordinates": [31.3116504, -26.034500] } },
+{ "type": "Feature", "properties": { "Site": "Big Bend", "Family Code": "BC(BMS)86", "Region": "Lubombo" }, "geometry": { "type": "Point", "coordinates": [31.9594845, -26.454672] } },
+{ "type": "Feature", "properties": { "Site": "Mankayane", "Family Code": "BC(BMS)F03", "Region": "Manzini" }, "geometry": { "type": "Point", "coordinates": [31.04485, -26.4150] } }
+
+]
+}
